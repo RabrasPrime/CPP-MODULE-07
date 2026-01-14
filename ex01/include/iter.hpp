2564592fb0471123c8396a7f73e65800de4ce9be
tjooris <tom.jooris@hotmail.fr>
2026-01-14 16:18:56 +0100

    patch cpp07

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -14,7 +14,14 @@ void    printElement(T &element)
 }
 
 template <typename T>
-void    iter(T *array, size_t length, void (*func)(T &))
+void    iter(T *array, const size_t length, void (*func)(T &))
+{
+	for (size_t i = 0; i < length; i++)
+        func(array[i]);
+}
+
+template <typename T>
+void    iter(T *array, const size_t length, void (*func)(const T &))
 {
 	for (size_t i = 0; i < length; i++)
         func(array[i]);
